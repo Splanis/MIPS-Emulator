11573a303104286f954c7a7270ee8bd53803bc76
splanis <mariosgalanis@outlook.com>
2020-10-23 20:15:06 +0300

    close file action added to reducer

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -3,11 +3,12 @@ import * as actions from "./actionTypes";
 export const reducer = (state, { type, payload }) => {
     switch (type) {
         case actions.OPEN_FILE:
-            return { ...state, editor: payload };
+            return { ...state, editor: payload.editor, openFile: payload.fileName };
         case actions.SET_EDITOR:
             return { ...state, editor: payload };
+        case actions.CLOSE_FILE:
+            return { ...state, editor: "", openFile: null };
         case actions.CHANGE_THEME:
-            console.log("?");
             return { ...state, theme: payload };
         default:
             return state;
