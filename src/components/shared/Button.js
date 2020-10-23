cf2d4165a198d83fa28bdda90682a4bb19e7a454
splanis <mariosgalanis@outlook.com>
2020-10-23 20:13:58 +0300

    added disabled option to Button, cursor on hover

--- src/components/shared/Button.js
+++ src/components/shared/Button.js
@@ -5,11 +5,15 @@ export const StyledButton = styled.button`
     justify-content: center;
     align-items: center;
     background: #c0c0c0;
-    color: black;
+    color: ${(props) => (props.disabled ? "grey" : "black")};
     padding: 10px 5px;
     border: none;
     width: 120px;
     height: 35px;
     margin: 5px;
     font-size: 1rem;
+
+    &:hover {
+        cursor: ${(props) => (props.disabled ? "auto" : "cursor")};
+    }
 `;
