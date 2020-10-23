6a11587ba9951756542d628364086f25f8a589c5
splanis <mariosgalanis@outlook.com>
2020-10-23 21:13:18 +0300

    moved CodeMirror class from global to Editor

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -15,22 +15,48 @@ const Editor = () => {
     };
 
     return (
-        <StyledControlledEditor
-            onBeforeChange={handleChange}
-            value={state.editor}
-            options={{
-                lineWrapping: true,
-                lint: true,
-                mode: "",
-                lineNumbers: true,
-                theme: state.theme,
-            }}
-        />
+        <StyledEditor>
+            <StyledEditorSettings>
+                {state.openFile && <div>Open File: {state.openFile}</div>}
+            </StyledEditorSettings>
+            <StyledControlledEditor
+                onBeforeChange={handleChange}
+                value={state.editor}
+                options={{
+                    lineWrapping: true,
+                    lint: true,
+                    mode: "",
+                    lineNumbers: true,
+                    theme: state.theme,
+                }}
+            />
+        </StyledEditor>
     );
 };
 
-const StyledControlledEditor = styled(ControlledEditor)`
+const StyledEditor = styled.div`
     grid-area: editor;
+`;
+
+const StyledEditorSettings = styled.div`
+    display: flex;
+    align-items: center;
+    height: 35px;
+    background: #1a1a1a;
+    padding-left: 10px;
+`;
+
+const StyledControlledEditor = styled(ControlledEditor)`
+    height: 100%;
+
+    .CodeMirror {
+        min-height: 100%;
+    }
+
+    .CodeMirror-scroll {
+        margin: 0;
+        padding: 0;
+    }
 `;
 
 export default Editor;
