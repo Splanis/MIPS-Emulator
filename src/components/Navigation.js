580c95833f634191fb4c99482a3a214cb6b63408
splanis <mariosgalanis@outlook.com>
2020-10-23 20:43:46 +0300

    added file buttons to navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,42 +1,114 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import { Context } from "../context/Context";
 import * as actions from "../context/actionTypes";
-import styled from "styled-components";
 import { StyledButton } from "../components/shared/Button";
+import styled from "styled-components";
 
 const themes = ["material", "monokai"];
 
 const Navigation = () => {
     const [state, dispatch] = useContext(Context);
+    const [downloading, setDownloading] = useState(false);
+    const [fileName, setFileName] = useState("");
+    const [filesDropDown, setFilesDropDown] = useState(false);
+
+    const nameFile = () => {
+        setDownloading(true);
+    };
 
     const openFile = async (e) => {
         e.preventDefault();
         const reader = new FileReader();
+        reader.fileName = e.target.files[0].name;
         reader.onload = async (e) => {
             const text = e.target.result;
-            dispatch({ type: actions.OPEN_FILE, payload: text });
+            dispatch({
+                type: actions.OPEN_FILE,
+                payload: { editor: text, fileName: e.target.fileName },
+            });
         };
         reader.readAsText(e.target.files[0]);
+        setFilesDropDown(false);
+    };
+
+    const downloadFile = () => {
+        if (state.openFile || fileName) {
+            let file = new Blob([state.editor], { type: "text/plain;charset=utf-8" });
+            let atag = document.createElement("a");
+            atag.href = URL.createObjectURL(file);
+            atag.download = fileName;
+            atag.click();
+            setDownloading(false);
+            setFileName("");
+        }
+        setFilesDropDown(false);
+    };
+
+    const closeFile = () => {
+        dispatch({ type: actions.CLOSE_FILE });
+        setFilesDropDown(false);
     };
 
     const handleThemeChange = (e) => {
-        console.log(e);
         dispatch({ type: actions.CHANGE_THEME, payload: e.target.value });
     };
 
     return (
         <StyledNavigation>
-            <StyledUpload
-                id="upload-file"
-                type="file"
-                onChange={(e) => openFile(e)}
-                accept="text/plain"
-            />
-
-            <StyledLabel htmlFor="upload-file">Upload File</StyledLabel>
+            <Dropdown>
+                <DropDownButton onClick={() => setFilesDropDown((prev) => !prev)}>
+                    Files
+                </DropDownButton>
+                <DropwDownContent filesDropDown={filesDropDown}>
+                    <DropdownItems>
+                        <input
+                            style={{ display: "none" }}
+                            id="upload-file"
+                            type="file"
+                            onChange={(e) => {
+                                openFile(e);
+                                setFileName(e.target.files[0].name);
+                            }}
+                            accept="text/plain"
+                        />
+                        <StyledLabel htmlFor="upload-file">Open</StyledLabel>
+                    </DropdownItems>
+                    <DropdownItems>
+                        <StyledButton
+                            disabled={state.openFile || fileName ? false : true}
+                            onClick={downloadFile}
+                        >
+                            Save
+                        </StyledButton>
+                    </DropdownItems>
+                    <DropdownItems>
+                        {downloading ? (
+                            <>
+                                <StyledInput
+                                    placeholder="filename"
+                                    value={fileName}
+                                    onChange={(e) => setFileName(e.target.value)}
+                                />
+                                <DownloadButton onClick={downloadFile}>+</DownloadButton>
+                            </>
+                        ) : (
+                            <StyledButton onClick={nameFile}>Save As</StyledButton>
+                        )}
+                    </DropdownItems>
+                    <DropdownItems>
+                        <StyledButton
+                            disabled={state.openFile || fileName ? false : true}
+                            onClick={closeFile}
+                        >
+                            Close
+                        </StyledButton>
+                    </DropdownItems>
+                </DropwDownContent>
+            </Dropdown>
 
             <StyledButton>Run</StyledButton>
+
             <StyledButton>Step Forward</StyledButton>
 
             <SelectTheme onChange={handleThemeChange} value={state.theme}>
@@ -46,6 +118,8 @@ const Navigation = () => {
                     </option>
                 ))}
             </SelectTheme>
+
+            {state.openFile && <div>Open File: {state.openFile}</div>}
         </StyledNavigation>
     );
 };
@@ -60,10 +134,6 @@ const StyledNavigation = styled.div`
     padding: 1rem;
 `;
 
-const StyledUpload = styled.input`
-    display: none;
-`;
-
 const StyledLabel = styled.label`
     display: flex;
     justify-content: center;
@@ -76,10 +146,34 @@ const StyledLabel = styled.label`
     height: 35px;
     margin: 5px;
     font-size: 1rem;
+
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
+const StyledInput = styled.input`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #c0c0c0;
+    color: black;
+    padding: 10px 5px;
+    border: none;
+    width: 120px;
+    height: 35px;
+    margin: 5px;
+    font-size: 1rem;
+    position: relative;
+`;
+
+const DownloadButton = styled.button`
+    position: absolute;
+`;
+
 const SelectTheme = styled.select`
-    margin: 10px;
     width: 130px;
-    padding: 5px;
+    margin: 5px;
     font-size: 16px;
     border: 1px solid #ccc;
     height: 35px;
@@ -98,6 +192,34 @@ const SelectTheme = styled.select`
         outline-color: red;
     }
 `;
+
+const Dropdown = styled.div`
+    margin: 5px;
+`;
+
+const DropwDownContent = styled.div`
+    display: ${(props) => (props.filesDropDown ? "flex" : "none")};
+    position: absolute;
+    background-color: #f9f9f9;
+    min-width: 140px;
+    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+    z-index: 10;
+    flex-direction: column;
+`;
+
+const DropDownButton = styled(StyledButton)`
+    margin: 0;
+`;
+
+const DropdownItems = styled.div`
+    text-decoration: none;
+    background: #c0c0c0;
+    border: none;
+    font-size: 1rem;
+
+    &:hover {
+        cursor: pointer;
+    }
 `;
 
 export default Navigation;
