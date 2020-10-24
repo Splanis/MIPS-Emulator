00353d5bc8e16db3b7204841a6f08e9788ba7b7f
splanis <mariosgalanis@outlook.com>
2020-10-24 18:53:19 +0300

    added new and change file actions

--- src/context/actionTypes.js
+++ src/context/actionTypes.js
@@ -5,3 +5,5 @@ export const CLOSE_FILE = 3;
 export const CHANGE_FONT_SIZE = 4;
 export const CHANGE_NUMERAL_SYSTEM = 5;
 export const TOGGLE_SIDE_BAR = 6;
+export const NEW_FILE = 7;
+export const CHANGE_FILE = 8;
