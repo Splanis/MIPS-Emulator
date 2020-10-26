baeab4f1421d975f90a2a96ddb2873a472c364a0
splanis <mariosgalanis@outlook.com>
2020-10-26 14:57:02 +0200

    removed strict mode

--- src/index.js
+++ src/index.js
@@ -3,18 +3,16 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 import { ContextProvider } from "./context/Context";
-import GlobalTheme from "./components/shared/GlobalTheme";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { theme } from "./components/Theme/Theme";
+import GlobalTheme from "./components/shared/GlobalTheme";
 
 ReactDOM.render(
-    <React.StrictMode>
         <ThemeProvider theme={theme}>
             <ContextProvider>
                 <GlobalTheme />
                 <App />
             </ContextProvider>
         </ThemeProvider>
-    </React.StrictMode>,
     document.getElementById("root")
 );
